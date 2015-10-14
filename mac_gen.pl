b7e8911422741a46988cf3abdbc1ee105a09941d
Derrick <derrick.walton@ymail.com>
2015-10-14 12:45:20 -0700

    Cleaned up script.

--- mac_gen.pl
+++ mac_gen.pl
@@ -1,14 +1,11 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
 use diagnostics;
-
 my @choices = qw( 0 1 2 3 4 5 6 7 8 9 A B C D E F );
 my $counter = 12;
 my $counter2 = 1;
 my @mac;
-
 while ($counter > 0) { 
 	my $num = int(rand(15));
 	my $entry = $choices[$num];
@@ -32,22 +29,18 @@ foreach my $bar (@mac) {
 	}
 }
 
-#Another way to generate a mac. 
-
+#The revised way of doing all of this stuff. 
 my %mac2;
 $counter = 0; 
-
 foreach my $foo (0..15){
 	foreach my $bar (0..15){
 		$mac2{$counter} = "$choices[$foo]$choices[$bar]";
 		$counter += 1;
-		
 	}
 }
 
 foreach my $foo (0..5) { 
 	$counter =int(rand(255));
-	#print $mac2{$counter};
 	if ($foo lt 5) { 
 		print "$mac2{$counter}:";
 	} else { 
