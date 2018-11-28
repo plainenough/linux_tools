908fe945e7ac751854a4a38d18caffeabc3b0e5a
Derrick <plainenough@users.noreply.github.com>
2018-11-28 16:50:32 -0700

    Update lock - remove comment

--- lock.sh
+++ lock.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-
-#Generate a list of all of the pictures for the lock screen. 
-
+#!/bin/bash 
 PICT=$(for i in `ls /home/dwalton/lockscreenimages/`; do echo /home/dwalton/lockscreenimages/$i; done | sort -R | head -1);
-
 #now to lock the screen. 
-
 i3lock -i $PICT -t;
