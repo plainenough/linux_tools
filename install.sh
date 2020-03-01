29e7487dd6b50eba87bce82d77003e95ee8b3535
Derrick Walton <derrick@tosstalk.com>
2020-03-01 14:15:35 -0700

    correct i3lock-fancy install

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@
 
 #load script 
 echo "Installing Load script";
-cp ~dwalton/linux_tools/load /bin/load && chown root: /bin/load && chmod 755 /bin/load && echo "Success" || echo "failed";
-
+cp ~dwalton/linux_tools/load /usr/local/bin/load && chown root: /usr/local/bin/load && chmod 755 /usr/local/bin/load && echo "Success" || echo "failed";
 #symlink settings
 echo "Creating some symlinks";
 #Symlink xresources
-echo "Creating symlink for .Xresources"; 
+echo "Creating symlink for .Xresources";
+rm -f ~dwalton/.Xresources;
 ln -s ~dwalton/linux_tools/.Xresources ~dwalton/.Xresources; chown dwalton: ~dwalton/.Xresources; stat ~dwalton/.Xresources \
 && echo "Symlink for Xresources success" || echo "Symlink for Xresources has failed"; 
 #Symlink i3config. 
@@ -26,12 +26,9 @@ ln -s ~dwalton/linux_tools/.vimrc ~dwalton/.vimrc; chown dwalton: ~dwalton/.vimr
 || echo "Vimrc symlink failed";
 #Symlink for bashrc
 echo "Creating Symlink for bashrc";
+rm -f ~dwalton/.bashrc;
 ln -s ~dwalton/linux_tools/.bashrc ~dwalton/.bashrc; chown dwalton: ~dwalton/.bashrc; stat ~dwalton/.bashrc && echo "Bashrc Symlink has been created"\
 || echo "Bashrc symlink failed.";
 #Install lock screen. 
-mkdir -p /tmp/scripts/; cd /tmp/scripts/; git clone https://github.com/meskarune/i3lock-fancy.git; cd i3lock-fancy/; chmod -R 755 ./icons && \
-chmod 755 ./lock &&  cp ./lock /usr/local/bin/ && cp -r ./icons /usr/local/bin/ && echo "lock has been created" || echo "lock install failed.";
-#Symlink for .xinitrc
-#echo "Creating Symlink for xinitrc";
-#ln -s ~dwalton/linux_tools/.xinitrc ~dwalton/.xinitrc; chown dwalton: ~dwalton/.xinitrc; stat ~dwalton/.xinitrc && echo "xinitrc Symlink has been created"\
-#|| echo "xinitrc failed.";
+mkdir -p /tmp/scripts/; cd /tmp/scripts/; git clone https://github.com/meskarune/i3lock-fancy.git; cd i3lock-fancy/; make install && \
+echo "lock has been created" || echo "lock install failed."; rm -rf /tmp/scripts;
